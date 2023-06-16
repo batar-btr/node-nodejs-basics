74a5a8f08c4c10f132a6d1fabbd78912db6be8f1
batar-btr <btrn86@gmail.com>
2023-06-16 20:30:59 +0300

    feat: add cli solutions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
     // Write your code here 
+    const args = process.argv.slice(2);
+    args.forEach((arg, idx) => {
+        if (arg.startsWith('--')) {
+            console.log(`${arg} is ${args[idx + 1]}`);
+        }
+    })
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
     // Write your code here 
+    const prefix = 'RSS_';
+    
+    const envVars = process.env;
+
+    for(let key of Object.keys(envVars)) {
+        if(key.startsWith(prefix)) {
+            console.log(`${key}=${envVars[key]}`);
+        }
+    }
 };
 
 parseEnv();
