41de3b9eda61e6f90efd2ca64c2d923dd589e06d
batar-btr <btrn86@gmail.com>
2023-06-19 18:27:12 +0300

    feat: add child-process solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import { fork } from 'child_process';
+import url from 'url';
+import path from 'path';
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+    const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+    const filePath = path.join(__dirname, 'files', 'script.js');
+
+    const child = fork(filePath, args, {
+        stdio: [process.stdin, process.stdout, 'ipc']
+    });
+    
+    child.on('error', (err) => {
+        // This will be called with err being an AbortError if the controller aborts
+        console.error(err);
+    });
+
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( [1,2,3,4]);
